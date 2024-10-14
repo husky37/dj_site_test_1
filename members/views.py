23dcc69bf2572dd2d9129c811b5e176869079a0c
Artem <raspberry347@gmail.com>
2024-10-14 17:19:09 +0300

    Lesson 'Django Prepare Template'

--- members/views.py
+++ members/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 # Create your views here.
 
 
 def members(request):
-    template = loader.get_template('members_list.html')
-    return HttpResponse(template.render())
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': mymembers
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def page_not_found_views(request, exception):
